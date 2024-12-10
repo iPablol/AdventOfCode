297a67f66e309fdc784ec981f135a96919227b6e
iPablol <106445718+iPablol@users.noreply.github.com>
2024-12-10 19:35:20 +0100

    Added 2015 day 5

--- AdventOfCode/obj/Debug/net8.0-windows/AdventOfCode.AssemblyInfo.cs
+++ AdventOfCode/obj/Debug/net8.0-windows/AdventOfCode.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdventOfCode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bcb4930edf06504ad1d6b8b9dd4bbd913250d36")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b494190d553355704460fb02421d68fe45f6d421")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdventOfCode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdventOfCode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
